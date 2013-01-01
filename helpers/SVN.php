f95c3b835efb11ff1ab1a69cab82c6c7ced99d61
Krasimir Tsonev <krasimir@outset.ws>
2013-01-01 13:01:53 +0200

    Fixing bug in SVN log operation

--- helpers/SVN.php
+++ helpers/SVN.php
@@ -23,11 +23,11 @@
                 $cmdPreview = "svn checkout ".$this->app->source." ...";
                 return view("error.html", array(
                     "message" => "The destination, <strong>".$this->destination."</strong> doesn't have <strong>.svn</strong> folder, which probably means that there is no SVN initialized there.<br /><br />".view("form.command.html", array(
-                            "cmd" => $cmd,
-                            "cmdPreview" => $cmdPreview,
-                            "callback" => "/apps/".$this->app->id,
-                            "label" => "Perform 'svn checkout'"
-                        ))
+                        "cmd" => $cmd,
+                        "cmdPreview" => $cmdPreview,
+                        "callback" => "/apps/".$this->app->id,
+                        "label" => "Perform 'svn checkout'"
+                    ))
                 ));
             }
 
@@ -38,19 +38,28 @@
             $result = $shell->toString();
             // $result = file_get_contents(__DIR__."/../testdata/svninfo.xml"); // for testing purpose
             $xml = simplexml_load_string($result);
+            $currentRevision = 1;
             if(isset($xml->entry)) {
                 $attributes = $xml->entry->attributes();
-                $revisionsMarkup .= view("info.html", array("message" => "Current revision: ".$attributes->revision));
+                $currentRevision = $attributes->revision;
+                $revisionsMarkup .= view("info.html", array("message" => "Current revision: ".$currentRevision));
             }
 
             // Getting latest commits
-            $shell = new Shell("cd ".$this->destination." && svn log -v --xml --with-all-revprops -l 10 --username ".$this->app->user." --password ".$this->app->pass);
+            $startFromRevision = $currentRevision-10 < 1 ? 1 : $currentRevision-10;
+            $cmd = "cd ".$this->destination." && svn log -v -r ".$startFromRevision.":HEAD --non-interactive --xml --with-all-revprops --username ".$this->app->user." --password ".$this->app->pass;
+            $shell = new Shell($cmd);
             $result = $shell->toString();
             // $result = file_get_contents(__DIR__."/../testdata/svnlog.xml"); // for testing purpose
             $xml = simplexml_load_string($result);
+            $revisionsRows = array();
             $revisionsMarkup .= '<table class="table table-bordered">';
             if(isset($xml->logentry)) {
                 foreach($xml->logentry as $entry) {
+                    $revisionsRows []= $entry;
+                }
+                $revisionsRows = array_reverse($revisionsRows);
+                foreach($revisionsRows as $entry) {
                     $attributes = $entry->attributes();
                     $files = "";
                     if(isset($entry->paths)) {
@@ -61,14 +70,18 @@
                             }                            
                         }
                     }
-                    $revisionsMarkup .= view("revision.html", array(
+                    $tpl = "revision.html";
+                    if((int)$currentRevision == (int)(isset($attributes->revision) ? $attributes->revision : "")) {
+                        $tpl = "revision.current.html";
+                    }
+                    $revisionsMarkup .= view($tpl, array(
                         "revision" => isset($attributes->revision) ? $attributes->revision : "",
                         "author" => isset($entry->author) ? $entry->author : "",
                         "message" => isset($entry->msg) ? $entry->msg : "",
                         "date" => isset($entry->date) ? $entry->date : "",
                         "files" => $files,
                         "action-release" => view("form.command.html", array(
-                            "cmd" => isset($attributes->revision) ? "cd ".$this->destination." && svn update -r ".$attributes->revision." --username ".$this->app->user." --password ".$this->app->pass : "",
+                            "cmd" => isset($attributes->revision) ? "cd ".$this->destination." && svn update --non-interactive --force -r ".$attributes->revision." --username ".$this->app->user." --password ".$this->app->pass : "",
                             "cmdPreview" => "svn update -r ".$attributes->revision,
                             "callback" => "/apps/".$this->app->id,
                             "label" => "release"
